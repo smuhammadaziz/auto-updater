fe1ff3ef35539a55ae7e88e734feda85ea013482
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-04-21 13:36:25 +0500

    Bump version to 2.0.0 and update login form header text

--- app/src/components/loginPage/components/LoginForm.jsx
+++ app/src/components/loginPage/components/LoginForm.jsx
@@ -29,7 +29,7 @@ function LoginForm({
 		<div className="bg-white p-12 rounded-xl shadow-2xl w-full max-w-lg transform transition-transform duration-300">
 			<h2 className="text-4xl font-bold mb-6 text-center text-gray-800">
 				{/* {content[language].login.login} */}
-				Login
+				Login form page page
 			</h2>
 
 			<UserTypeDropdown
